9f868a14395b92dd41c485fe3a35e7bd50030d5b
Hemil13 <157737404+Hemil13@users.noreply.github.com>
2025-12-22 17:32:38 +0530

    Intent, Toast, Snackbar

--- app/src/main/java/com/example/indusinternship/MainActivity.java
+++ app/src/main/java/com/example/indusinternship/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.indusinternship;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +14,38 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView create_account;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        create_account = findViewById(R.id.create_new_account);
+        login = findViewById(R.id.login_button);
+
+        create_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
         });
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this, "Login Succesfully", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_LONG).show();
+                Snackbar.make(view, "Login Successfully", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
