4d7b33cde24790c3979accf5b3ca64c862847324
Hemil13 <157737404+Hemil13@users.noreply.github.com>
2026-01-01 17:13:53 +0530

    SQLite Signup

--- app/src/main/java/com/example/indusinternship/SignupActivity.java
+++ app/src/main/java/com/example/indusinternship/SignupActivity.java
@@ -2,6 +2,8 @@ package com.example.indusinternship;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,10 +24,16 @@ public class SignupActivity extends AppCompatActivity {
 
     SharedPreferences sp;
 
+    SQLiteDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        db = openOrCreateDatabase("IndusInternship.db", MODE_PRIVATE, null);
+        String createTable = "CREATE TABLE IF NOT EXISTS user(userid INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(100), email VARCHAR(100), contact VARCHAR(10), password VARCHAR(20))";
+        db.execSQL(createTable);
 
         name = findViewById(R.id.signup_name);
         email = findViewById(R.id.signup_email);
@@ -44,34 +52,51 @@ public class SignupActivity extends AppCompatActivity {
                 if(name.getText().toString().trim().equals("")){
                     name.setError("Invalid Name");
                 }
-                if(!email.getText().toString().trim().matches(email_pattern)){
+                else if(!email.getText().toString().trim().matches(email_pattern)){
                     email.setError("Invalid Email");
                 }
-                if(contact.getText().toString().trim().equals("")){
+                else if(contact.getText().toString().trim().equals("")){
                     contact.setError("Enter a contact number");
                 }
-                if(contact.getText().toString().trim().length()<10){
+                else if(contact.getText().toString().trim().length()<10){
                     contact.setError("Invalid Contact Number");
                 }
-                if(password.getText().toString().trim().equals("")){
+                else if(password.getText().toString().trim().equals("")){
                     password.setError("Enter a Password");
                 }
-                if(password.getText().toString().trim().length()<8){
-                    password.setError("Password must be 8 characters long");
-                }
-                if(!cnfpassword.getText().toString().trim().matches(password.getText().toString().trim())){
+//                if(password.getText().toString().trim().length()<8){
+//                    password.setError("Password must be 8 characters long");
+//                }
+                else if(!cnfpassword.getText().toString().trim().matches(password.getText().toString().trim())){
                     cnfpassword.setError("Password doesn't match");
                 }
                 else{
 
-                    sp.edit().putString(ConstantSp.name, name.getText().toString()).commit();
-                    sp.edit().putString(ConstantSp.email, email.getText().toString()).commit();
-                    sp.edit().putString(ConstantSp.contact, contact.getText().toString()).commit();
-                    sp.edit().putString(ConstantSp.password, password.getText().toString()).commit();
+                    String checkUser = "SELECT * FROM user WHERE email='"+email.getText().toString()+"' OR contact='"+contact.getText().toString()+"'";
+                    Cursor cursor = db.rawQuery(checkUser, null);
 
-                    Toast.makeText(SignupActivity.this, "Signup Successful", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(SignupActivity.this, ProfileActivity.class);
-                    startActivity(intent);
+                    if(cursor.getCount()>0){
+                        Toast.makeText(SignupActivity.this, "User Already Exist", Toast.LENGTH_SHORT).show();
+//                        return;
+                    }
+                    else {
+                        String insertUser = "INSERT INTO user VALUES(NULL,'"+name.getText().toString()+"','"+email.getText().toString()+"','"+contact.getText().toString()+"','"+password.getText().toString()+"')";
+                        db.execSQL(insertUser);
+
+
+                        sp.edit().putString(ConstantSp.name, name.getText().toString()).commit();
+                        sp.edit().putString(ConstantSp.email, email.getText().toString()).commit();
+                        sp.edit().putString(ConstantSp.contact, contact.getText().toString()).commit();
+                        sp.edit().putString(ConstantSp.password, password.getText().toString()).commit();
+
+                        Toast.makeText(SignupActivity.this, "Signup Successful", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(SignupActivity.this, ProfileActivity.class);
+                        startActivity(intent);
+                    }
+
+
+
+
                 }
             }
         });
